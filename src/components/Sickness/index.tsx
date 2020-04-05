42f28fe6e3b60bc30052c1f70e3b545c319d98ea
Sebastián Valencia <sebastian.valencian@gmail.com>
2020-04-04 19:21:38 -0500

    [006] Feat - Create Sickness component

---
+++ src/components/Sickness/index.tsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom"
+
+type SicknessState = {
+    a : boolean;
+}
+
+class Sickness extends Component<{}, SicknessState> {
+
+ 
+
+
+    render() {
+
+        
+        return (
+            <div>
+                <h1>Hola Sickness</h1>
+
+            </div>
+            
+        )
+    }
+
+}
+
+export default Sickness;
